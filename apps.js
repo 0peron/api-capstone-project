d7c840996b59a62604dd238c668481a7361c283d
0peron <phubbard89@gmail.com>
2017-01-02 19:00:47 -0500

    cleaning up code

--- apps.js
+++ apps.js
@@ -1,30 +1,34 @@
 "use strict";
 $(document).ready(function () {
+    $('.overlay').hide();
+    $('.modal').hide();
+    $('.albumList').hide();
     // step 1 define functions
     // a) get input from user
     $('.js-search-form').submit(function (event) {
+        $('.albumList').show();
         event.preventDefault();
-        var artistName = $('.js-query').val();
-        //        console.log(artistName);
-        var searchType = $('#js-drop').val();
-        //        console.log(searchType);
-        getDataApi(artistName, searchType);
+
+        var songTitle = $('.js-query').val();
+        console.log(songTitle);
+        getDataApi(songTitle);
     });
+    $('.overlay').on('click', function () {
+        $('.overlay').hide();
+        $('.modal').hide();
+    });
+
     // b) make api call
-    function getDataApi(artistName, searchType) {
-        if (searchType == 'artist') {
-            var urlbuild = 'https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1/matcher.lyrics.get?q_artist=' + artistName;
-        } else if (searchType == 'song') {
-            var urlbuild = 'https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1/matcher.lyrics.get?q_track=' + artistName;
-        }
+    function getDataApi(songTitle) {
+
         var datacards = $.ajax({
-            url: urlbuild,
+            url: 'https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1/track.search?f_has_lyrics=1&page=1&page_size=15&q_track=' + songTitle,
             type: 'GET',
             data: "",
             dataType: 'json',
-            success: function (artistData) {
-                console.log(atristData);
-                displaySearch(atristData);
+            success: function (songData) {
+                console.log(songData);
+                displaySearch(songData);
             },
 
             error: function (err) {
@@ -36,18 +40,56 @@ $(document).ready(function () {
 
         });
     }
-    // c) display results
-    function displaySearch(videosArray) {
-        var addHTML = "";
-        $.each(videosArray, function (videoArrayKey, videoArrayValue) {
-            addHTML += "<li>";
-            addHTML += "<p>" + videoArrayValue.snippet.title + "</p>";
-            addHTML += "<a href='https://www.youtube.com/watch?v=" + videoArrayValue.id.videoId + "' target='_blank'>";
-            addHTML += "<img src='" + videoArrayValue.snippet.thumbnails.medium.url + "'/>";
-            addHTML += "</a>";
-            addHTML += "</li>";
+
+    function getLyrics(trackId) {
+        var datacards = $.ajax({
+            url: 'https://musixmatchcom-musixmatch.p.mashape.com/wsr/1.1/track.lyrics.get?track_id=' + trackId,
+            type: 'GET',
+            data: "",
+            dataType: 'json',
+            success: function (lyricData) {
+                displayLyrics(lyricData.lyrics_body, trackId);
+            },
+
+            error: function (err) {
+                console.log(err);
+            },
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("X-Mashape-Key", "bpwSKV4BRxmshy47RVabKWp5yLPnp1cmSdLjsnxen7reY3IUGq");
+            }
+
         });
-        $('.js-search-results').html(addHTML);
     }
 
+    // c) display results
+    function displaySearch(songData) {
+        $('.modal').hide();
+        var addHTML = "";
+        $.each(songData, function (songArrayKey, songArrayValue) {
+            console.log(getLyrics(songArrayValue.track_id));
+            var getlyricsClickFunction = '$("#' + songArrayValue.track_id + ', .overlay").show()';
+            addHTML += "<li>";
+            addHTML += "<h2>" + songArrayValue.artist_name + "</h2>";
+            addHTML += "<p>" + songArrayValue.album_name + "</p>";
+            addHTML += "<img src='" + songArrayValue.album_coverart_100x100 + "'/>";
+            addHTML += "<p class='mod' onclick='" + getlyricsClickFunction + "'>" + songArrayValue.track_name + "</p>";
+            addHTML += "</li>";
+        });
+        $('.albums ul').html(addHTML);
+
+    }
+
+    function displayLyrics(lyricData, trackId) {
+        var addHTML = "";
+        var hidelyricsClickFunction = '$("#' + trackId + ', .overlay").hide()';
+
+
+        //        var niceOutput = lyricData.replace(/([\n])\w+/g, "<br />");
+        var niceOutput = lyricData.replace(/\n/g, "<br />");
+
+        addHTML += "<div class='modal' id='" + trackId + "' onclick='" + hidelyricsClickFunction + "'>";
+        addHTML += "<p>" + niceOutput + "</p>";
+        addHTML += "</div>";
+        $('.modal_wrapper').append(addHTML);
+    }
 });
